fa7a3e62a7452109917b6063e08bc960c05dc6fb
chauct <caothichau222@gmail.com>
2022-10-29 06:06:33 +0700

    edit header

--- src/common/components/Header/index.jsx
+++ src/common/components/Header/index.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import styles from "./style.module.css";
-import logo from "assets/img/Logo-light.png";
+import logoLight from "assets/img/Logo-light.png";
 import logoDark from "assets/img/Logo-dark.png";
 import { NavLink, useHistory } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
@@ -12,8 +12,7 @@ function Header() {
   const userProfile = useSelector((state) => state.auth.profile);
 
   const [header, setHeader] = useState(false);
-  const [color, setColor] = useState(false);
-  const [navbarLogo, setNavbarLogo] = useState(logo);
+  const [navbarLogo, setNavbarLogo] = useState(logoLight);
   const history = useHistory();
 
   const [isMobile, setIsMobile] = useState(false);
@@ -67,54 +66,28 @@ function Header() {
     if (userProfile) {
       return (
         <Dropdown overlay={menu} placement="bottom" arrow>
-          <span className={color ? `${styles.color} ` : `${styles.nav_link}`}>
-            Hi, {userProfile.hoTen}
-          </span>
+          <span className={styles.nav_link}>Hi, {userProfile.hoTen}</span>
         </Dropdown>
       );
     }
     return (
-      <div
-        className={isMobile ? `${styles.nav_link_toggle}` : `${styles.login}`}
-      >
-        <button
-          onClick={goToSignin}
-          className={color ? `${styles.btn_dark} ` : `${styles.btn_login}`}
-        >
-          Đăng nhập
-        </button>
-      </div>
+      <button onClick={goToSignin} className={styles.btn_login}>
+        Đăng nhập
+      </button>
     );
   };
 
   const changeBackgroundHeader = () => {
     if (window.scrollY >= 80) {
       setHeader(true);
-      setColor(true);
-    } else {
-      setHeader(false);
-      setColor(false);
-    }
-  };
-
-  useEffect(() => {
-    changeBackgroundHeader();
-    window.addEventListener("scroll", changeBackgroundHeader);
-  });
-
-  //logo scroll function
-  const changeLogo = () => {
-    if (window.scrollY >= 60) {
       setNavbarLogo(logoDark);
     } else {
-      setNavbarLogo(logo);
+      setHeader(false);
+      setNavbarLogo(logoLight);
     }
   };
 
-  useEffect(() => {
-    changeLogo();
-    window.addEventListener("scroll", changeLogo);
-  });
+  window.addEventListener("scroll", changeBackgroundHeader);
 
   return (
     <>
@@ -128,6 +101,7 @@ function Header() {
             <span onClick={goToHome} className={styles.logo}>
               <img src={navbarLogo} alt="logo" />
             </span>
+
             <div
               className={
                 isMobile ? `${styles.nav_link_toggle}` : `${styles.menu}`
@@ -138,55 +112,33 @@ function Header() {
             >
               <ul className={styles.menu_list}>
                 <li>
-                  <NavLink
-                    to="/"
-                    className={
-                      color ? `${styles.color} ` : `${styles.nav_link}`
-                    }
-                  >
+                  <NavLink to="/" className={styles.nav_link}>
                     Lịch chiếu
                   </NavLink>
                 </li>
 
                 <li>
-                  <NavLink
-                    to="/theater"
-                    className={
-                      color ? `${styles.color} ` : `${styles.nav_link}`
-                    }
-                  >
+                  <NavLink to="/theater" className={styles.nav_link}>
                     Cụm rạp
                   </NavLink>
                 </li>
                 <li>
-                  <NavLink
-                    to="/news"
-                    className={
-                      color ? `${styles.color} ` : `${styles.nav_link}`
-                    }
-                  >
+                  <NavLink to="/news" className={styles.nav_link}>
                     Tin tức
                   </NavLink>
                 </li>
                 <li>
-                  <NavLink
-                    to="/detail/:id"
-                    className={
-                      color ? `${styles.color} ` : `${styles.nav_link}`
-                    }
-                  >
+                  <NavLink to="/detail/:id" className={styles.nav_link}>
                     Ứng dụng
                   </NavLink>
                 </li>
               </ul>
             </div>
+
             {renderUserInfo()}
+
             <button
-              className={
-                color
-                  ? `${styles.toggle_dark} ${styles.toggle} `
-                  : `${styles.toggle}`
-              }
+              className={styles.toggle}
               onClick={() => {
                 setIsMobile(!isMobile);
               }}
